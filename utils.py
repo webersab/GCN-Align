e27dea8d761a58196253fe529c595d08195584a9
Sabine Weber <s1782911@ebirah.inf.ed.ac.uk>
2019-04-14 12:01:20 +0100

    less printouts, same fun

--- utils.py
+++ utils.py
@@ -84,7 +84,6 @@ def loadfile(fn, num=1):
     with open(fn, encoding='utf-8') as f:
         for line in f:
             th = line[:-1].split('\t')
-            print(str(th))
             x = []
             for i in range(num):
                 x.append(int(th[i]))
